791ea54c42ad15d140e0ebd861c18097c7c6c787
Mz427 <53973695+Mz427@users.noreply.github.com>
2021-06-13 23:45:19 +0800

    modified:   api_mian.py 	modified:   python_repo.html

--- api_mian.py
+++ api_mian.py
@@ -18,10 +18,20 @@ print(f"Total repositories: {response_dict['total_count']}")
 #['total_count', 'incomplete_results', 'items']
 
 repo_dicts = response_dict["items"]
-repo_names, stars = [], []
+repo_names, stars, labels, repo_links = [], [], [], []
 for i in repo_dicts:
+    repo_name = i['name']
     repo_names.append(i['name'])
     stars.append(i['stargazers_count'])
+
+    owner = i['owner']['login']
+    description = i['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+
+    repo_url = i['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
 
 #repo_dict = repo_dicts[0]
 #print(f"\nKeys: {len(repo_dict)}")
@@ -42,17 +52,35 @@ for i in repo_dicts:
 
 data = [{
     'type' : 'bar',
-    'x' : repo_names,
+    #'x' : repo_names,
+    'x' : repo_links,
     'y' : stars,
+    'hovertext' : labels,
     'marker' : {
         'color' : 'rgb(60, 100, 150)',
-        '' : {}
-    }
+        'line' : {
+            'width' : 1.5,
+            'color' : 'rgb(25, 25, 25)'
+            }
+        },
+    'opacity' : 0.6
     }]
 my_layout = {
         'title' : 'GitHub上最受欢迎的项目',
-        'xaxis' : {'title' : 'Repository'},
-        'yaxis' : {'title' : 'Stars'}
+        'titlefont' : {'size' : 28},
+        'xaxis' : {
+            'title' : 'Repository',
+            'titlefont' : {'size' : 24},
+            'tickfont' : {'size' : 14}
+            },
+        'yaxis' : {
+            'title' : 'Stars',
+            'titlefont' : {'size' : 24},
+            'tickfont' : {'size' : 14}
+            }
         }
-fig = {'data' : data, 'layout' : my_layout}
+fig = {
+    'data' : data,
+    'layout' : my_layout
+    }
 offline.plot(fig, filename='python_repo.html')
